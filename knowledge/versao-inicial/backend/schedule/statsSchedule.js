703d5ffbf9e760a8454000efbd3494420a3aff52
[cunhamuril] <[murilo.sant@hotmail.com]>
2019-09-03 12:31:06 -0300

    finalizado backend

---
+++ knowledge/versao-inicial/backend/schedule/statsSchedule.js
@@ -0,0 +1,30 @@
+const schedule = require('node-schedule')
+
+module.exports = app => {
+  schedule.scheduleJob('*/1 * * * *', async function () {
+    usersCount = await app.db('users').count('id').first()
+    categoriesCount = await app.db('categories').count('id').first()
+    articlesCount = await app.db('articles').count('id').first()
+
+    const { Stat } = app.api.stat
+
+    const lastStat = await Stat.findOne({}, {}, {
+      sort: { 'createdAt': -1 }
+    })
+
+    const stat = new Stat({
+      users: usersCount.count,
+      categories: categoriesCount.count,
+      articles: articlesCount.count,
+      createdAt: new Date()
+    })
+
+    const changeUsers = !lastStat || stat.users !== lastStat.users
+    const changeCategories = !lastStat || stat.categories !== lastStat.categories
+    const changeArticles = !lastStat || stat.articles !== lastStat.articles
+
+    if (changeUsers || changeCategories || changeArticles) {
+      stat.save().then(() => console.log('[Stats] Estatísticas atualizadas!'))
+    }
+  })
+}
