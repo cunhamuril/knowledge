4a6ebec62fc24478bbdd673d02f7cd672aa45cff
[cunhamuril] <[murilo.sant@hotmail.com]>
2019-09-20 16:45:17 -0300

    user signup
    pt1

--- knowledge/README.md
+++
@@ -1,5 +0,0 @@
-# Projeto Base de Conhecimento
-## Curso Web Moderno
-### COD3R Cursos Online / Udemy
-
-#### Backend com NodeJS e Frontend com VueJS
--- knowledge/versao-inicial/frontend/src/main.js
+++ knowledge/versao-inicial/frontend/src/main.js
@@ -1,9 +1,21 @@
+import 'font-awesome/css/font-awesome.css'
 import Vue from 'vue'
 
 import App from './App'
 
+import './config/bootstrap'
+import './config/msgs'
+import store from './config/store'
+import router from './config/router'
+
 Vue.config.productionTip = false
 
+// TEMPORÁRIO!
+// para qualquer requisição que o axios fizer, automaticamente terá um header com o nome Authorization
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6Ik11cmlsbyBTLiBDdW5oYSIsImVtYWlsIjoibXVyaWxvLnNhbnRAaG90bWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTY4OTkxODAxLCJleHAiOjE1NjkyNTEwMDF9.gWQcA9eUsFNEM5m7UbvqctqU10wH6IVA29TFd7IqkLY'
+
 new Vue({
+  store,
+  router,
   render: h => h(App)
 }).$mount('#app')
